b125c272760e5d94ef2b172b62a6907584a8a72f
yangrufeng <251814590@163.com>
2020-09-21 09:17:44 +0800

    Update meta.js

--- meta.js
+++ meta.js
@@ -74,6 +74,11 @@ module.exports = {
       type: 'confirm',
       message: 'Install vue-router?',
     },
+    vuex: {
+      when: 'isNotTest',
+      type: 'confirm',
+      message: 'Install vuex?',
+    },
     lint: {
       when: 'isNotTest',
       type: 'confirm',
